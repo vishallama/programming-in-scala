1ce3a8baa1d5d427fd9011742529f6c29157b546
Vishal Lama <vishal.lama@gmail.com>
2017-01-08 06:54:08 -0700

    Add above method

--- src/main/scala/elements/Element.scala
+++ src/main/scala/elements/Element.scala
@@ -2,6 +2,12 @@ package elements
 
 abstract class Element {
   def contents: Array[String]
+
   def height: Int = contents.length
-  def width: Int = if (height == 0) 0 else contents(0).length
+
+  def width: Int =
+    if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    new ArrayElement(this.contents ++ that.contents)
 }
