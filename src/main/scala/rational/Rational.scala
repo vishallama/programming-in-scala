8b081ff5c89aedefe2c7269ae9548e6c5fa0e1a1
Vishal Lama <vishal.lama@gmail.com>
2017-01-10 10:09:52 -0700

    Mix in Ordered trait

--- src/main/scala/rational/Rational.scala
+++ src/main/scala/rational/Rational.scala
@@ -1,6 +1,6 @@
 package rational
 
-class Rational(n: Int, d: Int) {
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
   require(d != 0)
 
   private val g = gcd(n.abs, d.abs)
@@ -8,6 +8,9 @@ class Rational(n: Int, d: Int) {
   val denom: Int = d / g
 
   def this(n: Int) = this(n, 1)
+
+  override def compare(that: Rational): Int =
+    (this.numer * that.denom) - (that.numer * this.denom)
 
   def + (that: Rational): Rational =
     new Rational(
@@ -39,7 +42,7 @@ class Rational(n: Int, d: Int) {
   def / (i: Int): Rational =
     new Rational(numer, denom * i)
 
-  override def toString = numer + "/" + denom
+  override def toString: String = numer + "/" + denom
 
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
