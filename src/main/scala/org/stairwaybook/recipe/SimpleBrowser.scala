aaddee3f149cb6b5fac77ea13c6891ab325b8b4f
Vishal Lama <vishal.lama@gmail.com>
2017-01-25 15:57:00 -0700

    Add displayCategory to food browser

--- src/main/scala/org/stairwaybook/recipe/SimpleBrowser.scala
+++ src/main/scala/org/stairwaybook/recipe/SimpleBrowser.scala
@@ -4,4 +4,8 @@ object SimpleBrowser {
   def recipesUsing(food: Food): List[Recipe] =
     SimpleDatabase.allRecipes.filter(recipe =>
       recipe.ingredients.contains(food))
+
+  def displayCategory(category: SimpleDatabase.FoodCategory): Unit = {
+    println(category)
+  }
 }
