8626852c59a2d81d75282b59e9ce081fd8b51e36
Vishal Lama <vishal.lama@gmail.com>
2017-01-25 15:56:33 -0700

    Add food category to database

--- src/main/scala/org/stairwaybook/recipe/SimpleDatabase.scala
+++ src/main/scala/org/stairwaybook/recipe/SimpleDatabase.scala
@@ -9,4 +9,12 @@ object SimpleDatabase {
     allFoods.find(_.name == name)
 
   def allRecipes: List[Recipe] = List(FruitSalad)
+
+  case class FoodCategory(name: String, foods: List[Food])
+
+  private val categories = List(
+    FoodCategory("fruits", List(Apple, Orange)),
+    FoodCategory("misc", List(Cream, Sugar)))
+
+  def allCategories: List[FoodCategory] = categories
 }
