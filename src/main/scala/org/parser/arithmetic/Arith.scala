e92a4bd18c12e5c25c6693aa351ac7cb188b9672
Vishal Lama <vishal.lama@gmail.com>
2017-01-27 08:54:56 -0700

    Add a small arithmetic parser program

---
+++ src/main/scala/org/parser/arithmetic/Arith.scala
@@ -0,0 +1,9 @@
+package org.parser.arithmetic
+
+import scala.util.parsing.combinator._
+
+class Arith extends JavaTokenParsers {
+  def expr: Parser[Any] = term~rep("+"~term | "-"~term)
+  def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
+  def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
+}
---
+++ src/main/scala/org/parser/arithmetic/ParseExpr.scala
@@ -0,0 +1,8 @@
+package org.parser.arithmetic
+
+object ParseExpr extends Arith {
+  def main(args: Array[String]): Unit = {
+    println("input: " + args(0))
+    println(parseAll(expr, args(0)))
+  }
+}
