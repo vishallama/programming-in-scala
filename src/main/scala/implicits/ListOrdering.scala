ba59268169e82902568caf0ad1392ec8377a5b2f
Vishal Lama <vishal.lama@gmail.com>
2017-10-06 16:32:31 -0600

    Implement function to find max of a list using implicit ordering

---
+++ src/main/scala/implicits/ListOrdering.scala
@@ -0,0 +1,29 @@
+package implicits
+
+object ListOrdering {
+  def maxListOrdering[T](elements: List[T])
+                        (ordering: Ordering[T]): T =
+    elements match {
+      case List() =>
+        throw new IllegalArgumentException("empty list!")
+      case List(x) =>
+        x
+      case x :: rest =>
+        val maxRest = maxListOrdering(rest)(ordering)
+        if (ordering.gt(x, maxRest)) x
+        else maxRest
+    }
+
+  def maxListImpParm[T](elements: List[T])
+                        (implicit ordering: Ordering[T]): T =
+    elements match {
+      case List() =>
+        throw new IllegalArgumentException("empty list!")
+      case List(x) =>
+        x
+      case x :: rest =>
+        val maxRest = maxListImpParm(rest)(ordering)
+        if (ordering.gt(x, maxRest)) x
+        else maxRest
+    }
+}
