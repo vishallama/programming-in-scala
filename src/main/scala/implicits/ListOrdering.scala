51d0eb33a7cd002ed76c4501b1dae7d4fb155f3a
Vishal Lama <vishal.lama@gmail.com>
2017-10-06 20:34:59 -0600

    Implement function to find max of a list using context bound

--- src/main/scala/implicits/ListOrdering.scala
+++ src/main/scala/implicits/ListOrdering.scala
@@ -26,4 +26,16 @@ object ListOrdering {
         if (ordering.gt(x, maxRest)) x
         else maxRest
     }
+
+  def maxList[T : Ordering](elements: List[T]): T =
+    elements match {
+      case List() =>
+        throw new IllegalArgumentException("empty list!")
+      case List(x) =>
+        x
+      case x :: rest =>
+        val maxRest = maxList(rest)
+        if (implicitly[Ordering[T]].gt(x, maxRest)) x
+        else maxRest
+    }
 }
